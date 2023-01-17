9359d5cef297d7fc957a1e15405da3f237cededd
Aleks <aleks@JustComputer.local>
2023-01-17 10:52:47 +0200

    POST test

--- collection/tests.py
+++ collection/tests.py
@@ -17,9 +17,9 @@ class TestViews(TestCase):
         self.check_list = reverse('check-list')
 
         self.test_movie = Movie.objects.create(
-        image = 'image',
-        release_date = '2023-01-16',
-        slug = 'test-movie'
+        image='image',
+        release_date='2023-01-16',
+        slug='test-movie'
         )
 
     def test_collection_index_GET(self):
@@ -38,6 +38,15 @@ class TestViews(TestCase):
         self.assertTemplateUsed(res, "collection/movie-detail.html")
 
     def test_collection_check_list_GET(self):
-            res = self.client.get(self.check_list)
-            self.assertEquals(res.status_code, 200)
-            self.assertTemplateUsed(res, "collection/check-list.html")
+        res = self.client.get(self.check_list)
+        self.assertEquals(res.status_code, 200)
+        self.assertTemplateUsed(res, "collection/check-list.html")
+
+    def test_collection_movie_detail_comment_POST(self):
+        res = self.client.post(self.movie_detail_page, {
+            "user_name": "Test Name",
+            "user_email": "test@email.com",
+            "text": "test Comment"
+        })
+        self.assertEquals(res.status_code, 302)
+        self.assertEquals(self.test_movie.comments.first().text, 'test Comment')
